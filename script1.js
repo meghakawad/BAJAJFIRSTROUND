265b2da2434c0590001b7bb94e599178ebf80b9d
meghakawad <76148141+meghakawad@users.noreply.github.com>
2023-05-20 00:13:36 +0530

    Add files via upload

--- script1.js
+++ script1.js
@@ -3,22 +3,7 @@ const searchInput = document.getElementById('name-search');
 const designationFilter = document.getElementById('designation-filter');
 let developers = [];
 
-// Local copy of the JSON data
-const jsonData = [
-  {
-    "name": "John Doe",
-    "designation": "Frontend Developer",
-    "skills": ["HTML", "CSS", "JavaScript"]
-  },
-  {
-    "name": "Jane Smith",
-    "designation": "Backend Developer",
-    "skills": ["Python", "Java", "SQL"]
-  },
- 
-];
-
-
+// Render developers list
 function renderDevelopers(developers) {
   developerList.innerHTML = '';
 
@@ -34,18 +19,61 @@ function renderDevelopers(developers) {
     designation.textContent = `Designation: ${developer.designation}`;
     card.appendChild(designation);
 
+    const skills = document.createElement('p');
+    skills.textContent = `Skills: ${developer.skills.join(', ')}`;
+    card.appendChild(skills);
+
+    if (developer.projects) {
+      const projects = document.createElement('div');
+      projects.classList.add('projects');
+
+      developer.projects.forEach(project => {
+        const projectCard = document.createElement('div');
+        projectCard.classList.add('project-card');
+
+        const projectName = document.createElement('h4');
+        projectName.textContent = `Project: ${project.name}`;
+        projectCard.appendChild(projectName);
+
+        const projectDescription = document.createElement('p');
+        projectDescription.textContent = `Description: ${project.description}`;
+        projectCard.appendChild(projectDescription);
+
+        const teamMembers = document.createElement('p');
+        teamMembers.textContent = `Team Members: ${project.team.map(member => member.name).join(', ')}`;
+        projectCard.appendChild(teamMembers);
+
+        const tasks = document.createElement('ul');
+        tasks.classList.add('tasks');
+
+        project.tasks.forEach(task => {
+          const taskItem = document.createElement('li');
+          taskItem.textContent = `Task ${task.id}: ${task.name} (${task.status || 'N/A'})`;
+          tasks.appendChild(taskItem);
+        });
+
+        projectCard.appendChild(tasks);
+        projects.appendChild(projectCard);
+      });
+
+      card.appendChild(projects);
+    }
+
     developerList.appendChild(card);
   });
 }
 
+
+// Search developers by name
 function searchByName() {
   const searchTerm = searchInput.value.toLowerCase();
   const filteredDevelopers = developers.filter(developer =>
-    developer.name.toLowerCase().includes(searchTerm)
+    developer.name && developer.name.toLowerCase().includes(searchTerm)
   );
   renderDevelopers(filteredDevelopers);
 }
 
+// Filter developers by designation
 function filterByDesignation() {
   const selectedDesignation = designationFilter.value;
   if (selectedDesignation === '') {
@@ -58,7 +86,26 @@ function filterByDesignation() {
   }
 }
 
-developers = jsonData;
-renderDevelopers(developers);
-
+// Event listener for search input
 searchInput.addEventListener('input', searchByName);
+
+// Fetch JSON data from the file
+fetch('data.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    if (!Array.isArray(data.employees)) {
+      throw new Error('Data is not in the expected format');
+    }
+    developers = data.employees;
+    renderDevelopers(developers);
+    
+    // Event listener for designation filter
+    designationFilter.addEventListener('change', filterByDesignation);
+  })
+  .catch(error => console.error('Error fetching JSON data:', error));
+
